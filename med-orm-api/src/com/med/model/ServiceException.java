388d2f0d7ce314009dca79025b2c0d3702d66236
mykhailo.gevak <mykhailo.gevak@playtech.com>
2014-06-25 13:44:27 +0300

    -

---
+++ med-orm-api/src/com/med/model/ServiceException.java
@@ -0,0 +1,12 @@
+package com.med.model;
+
+
+public class ServiceException extends Exception{
+	public ServiceException(Exception ex) {
+		super(ex);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	
+}
--- med-server/src/com/med/server/ServerMain.java
+++ med-server/src/com/med/server/ServerMain.java
@@ -1,5 +1,6 @@
 package com.med.server;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -15,47 +16,62 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import com.google.inject.servlet.GuiceFilter;
 
 public class ServerMain {
-	
+
 	private static final String DEFAULT_PROPERTIES_RESOURCE = "/default.properties";
 
 	public static void main(String[] args) throws Exception {
 		Properties properties = loadProperties(args);
-		
-		ExtendedGuiceServletContextListener guiceServlet = new ExtendedGuiceServletContextListener(properties);
-        
+
+		ExtendedGuiceServletContextListener guiceServlet = new ExtendedGuiceServletContextListener(
+				properties);
+
 		Server appServer = createSimpleJettyServer(guiceServlet);
-    	appServer.start();
-    	
-    	System.out.println("Server running");
-        System.out.println("Hit return to stop...");
-        System.in.read();
-        System.out.println("Stopping server");
-        appServer.stop();
-        System.out.println("Server stopped");
-    }    
-    
+		appServer.start();
+
+		System.out.println("Server running");
+		System.out.println("Hit return to stop...");
+		System.in.read();
+		System.out.println("Stopping server");
+		appServer.stop();
+		System.out.println("Server stopped");
+	}
+
 	private static Properties loadProperties(String[] args) throws IOException {
 		InputStream inputStream = null;
-		
-		if (args == null || args.length == 0){
-			inputStream = ClassLoader.class.getResourceAsStream(DEFAULT_PROPERTIES_RESOURCE);
+		InputStream defaultStream = ClassLoader.class
+				.getResourceAsStream(DEFAULT_PROPERTIES_RESOURCE);
+
+		if (args != null && args.length > 0) {
+			inputStream = new FileInputStream(args[0]);
 		}
-		
+
 		Properties properties = new Properties();
-		properties.load(inputStream);
+		properties.load(defaultStream);
+		
+		if (inputStream != null) {
+			try {
+				properties.load(inputStream);
+			} catch (IOException ex) {
+				System.out.println("Can't load properties from the " + args[0]);
+			}
+		}
 		return properties;
 	}
 
-	private static Server createSimpleJettyServer(ExtendedGuiceServletContextListener guiceConfig) throws URISyntaxException{
+	private static Server createSimpleJettyServer(
+			ExtendedGuiceServletContextListener guiceConfig)
+			throws URISyntaxException {
 		URI uri = guiceConfig.getServerURI();
-    	Server server = new Server(uri.getPort());
-    	
-        ServletContextHandler root = new ServletContextHandler(server, uri.getPath());
-        
-        root.addEventListener(guiceConfig);
-        root.addFilter(GuiceFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
-        root.addServlet(DummyServlet.class, "/*");
-        
-        return server;
-    }
+		Server server = new Server(uri.getPort());
+
+		ServletContextHandler root = new ServletContextHandler(server,
+				uri.getPath());
+
+		root.addEventListener(guiceConfig);
+		root.addFilter(GuiceFilter.class, "/*",
+				EnumSet.allOf(DispatcherType.class));
+		root.addServlet(DummyServlet.class, "/*");
+
+		return server;
+	}
 }
